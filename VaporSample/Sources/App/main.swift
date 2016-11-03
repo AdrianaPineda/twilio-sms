462f0cdae2795869bbafca0c321db6a8b98b18c4
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-11-02 21:20:47 -0500

    Add post endpoint to create person

--- VaporSample/Sources/App/main.swift
+++ VaporSample/Sources/App/main.swift
@@ -1,4 +1,5 @@
 import Vapor
+import HTTP
 
 let drop = Droplet()
 
@@ -14,6 +15,17 @@ drop.get("hello") { request in
     return try drop.view.make("hello", [
       "name": name
     ])
+}
+
+drop.post("person") { request in
+  guard let name = request.data["name"]?.string, let city = request.data["city"]?.string else {
+    throw Abort.badRequest
+  }
+
+  return try Response(status: .created, json: JSON(node: [
+    "name": name,
+    "city": city
+   ]))
 }
 
 drop.resource("posts", PostController())
