7f429c5bb142c266baebcd4aad95a2475264bc55
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-11-02 21:02:21 -0500

    Add VaporSample

---
+++ VaporSample/Sources/App/main.swift
@@ -0,0 +1,17 @@
+import Vapor
+
+let drop = Droplet()
+
+drop.get { req in
+    return try drop.view.make("welcome", [
+    	"message": drop.localization[req.lang, "welcome", "title"]
+    ])
+}
+
+drop.get("hello") { request in
+  return "Hello World!"
+}
+
+drop.resource("posts", PostController())
+
+drop.run()
